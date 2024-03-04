69a572bbbfc64bcf8eb54310ac84c36c04d8a024
Memory Zulu <106831381+Moryzulu@users.noreply.github.com>
2024-03-04 01:16:24 -0800

    Create script.js
     added codes to make the the website interactive.

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Function to show a greeting pop-up
+function showGreeting() {
+    alert("Welcome to Spin & Sun! Enjoy your visit!");
+}
+
+// Add an event listener to a button (assuming you have a button in your HTML)
+document.getElementById("greetingButton").addEventListener("click", showGreeting);
